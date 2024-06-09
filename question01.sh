4d28f8c3e6e2687d163c66716c4d6ca909f1d453
vaibhavgarg-uniadl <a1918480@adelaide.edu.au>
2024-06-09 22:24:48 +0930

    Add files via upload

---
+++ question01.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/vaibhavgarg-uniadl/Comp-Sci-B.git
+cd Comp-Sci-B
+echo "This is a new file for Question 01" > file1.txt
+git add file1.txt
+git commit -m "Add file1.txt for Question 01"
+git push origin main
